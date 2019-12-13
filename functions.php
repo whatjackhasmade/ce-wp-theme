6fe4d2878e0a868edb14a0e1ddff39462edf7a05
Jack Pritchard <jack@noface.co.uk>
2019-12-13 19:45:14 +0000

    Remove old Timber context config

--- functions.php
+++ functions.php
@@ -62,8 +62,6 @@ class StarterSite extends Timber\Site
     public function __construct()
     {
         add_action('after_setup_theme', array($this, 'theme_supports'));
-        add_filter('timber/context', array($this, 'add_to_context'));
-        add_filter('timber/twig', array($this, 'add_to_twig'));
         add_filter('graphql_jwt_auth_secret_key', array($this, 'graphql_jwt'));
         add_action('init', array($this, 'register_post_types'));
         add_action('init', array($this, 'register_taxonomies'));
@@ -83,20 +81,6 @@ class StarterSite extends Timber\Site
     public function register_taxonomies()
     {
 
-    }
-
-    /** This is where you add some context
-     *
-     * @param string $context context['this'] Being the Twig's {{ this }}.
-     */
-    public function add_to_context($context)
-    {
-        $context['foo'] = 'bar';
-        $context['stuff'] = 'I am a value set in your functions.php file';
-        $context['notes'] = 'These values are available everytime you call Timber::context();';
-        $context['menu'] = new Timber\Menu();
-        $context['site'] = $this;
-        return $context;
     }
 
     public function theme_supports()
@@ -153,28 +137,6 @@ class StarterSite extends Timber\Site
 
         add_theme_support('menus');
     }
-
-    /** This Would return 'foo bar!'.
-     *
-     * @param string $text being 'foo', then returned 'foo bar!'.
-     */
-    public function myfoo($text)
-    {
-        $text .= ' bar!';
-        return $text;
-    }
-
-    /** This is where you can add your own functions to twig.
-     *
-     * @param string $twig get extension.
-     */
-    public function add_to_twig($twig)
-    {
-        $twig->addExtension(new Twig\Extension\StringLoaderExtension());
-        $twig->addFilter(new Twig\TwigFilter('myfoo', array($this, 'myfoo')));
-        return $twig;
-    }
-
 }
 
 new StarterSite();
